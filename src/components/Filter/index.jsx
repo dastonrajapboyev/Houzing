834ce0507f27b0a235606d15bfcc7cecdf444c37
dastonrajapboyev <dastonrajapboyev79@gmail.com>
2023-10-17 18:42:01 -0700

    filter input, button, icons created

--- src/components/Filter/index.jsx
+++ src/components/Filter/index.jsx
@@ -1,21 +1,31 @@
 import React from "react";
+import { Dropdown } from "antd";
 import { Container, Icons } from "./style";
 import { Input, Button } from "../Generic";
 
 const Filter = () => {
+  const menu = <div>test</div>
   return (
     <Container>
       <Input
         $icon={<Icons.Houses />}
         placeholder={"Enter an address, neighborhood, city, or ZIP code"}
       />
-      <Button type={"light"}>
-        {" "}
-        <Icons.Settings /> Search
-      </Button>
+
+      <Dropdown
+        menu={{ menu }}
+        placement="bottomRight"
+        arrow={{ pointAtCenter: true }}>
+        <div>
+          <Button type={"light"}>
+            <Icons.Settings /> Advanced
+          </Button>
+        </div>
+        <Button>bottomRight</Button>
+      </Dropdown>
       <Button>
         <Icons.Search />
-        Advanced
+        Search
       </Button>
     </Container>
   );
--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import Root from "./root";
 import RootContext from "./context";
+// import 'antd/dist/antd.css';
+import 'antd/dist/antd.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
