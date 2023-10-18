f6d355e647aa4aa8905f1a27f8022a91cc32b0ee
dastonrajapboyev <dastonrajapboyev79@gmail.com>
2023-10-17 23:29:50 -0700

    i'm getting some errors

--- src/components/Filter/index.jsx
+++ src/components/Filter/index.jsx
@@ -4,30 +4,32 @@ import { Container, Icons } from "./style";
 import { Input, Button } from "../Generic";
 
 const Filter = () => {
-  const menu = <div>test</div>
+  const menu = <div>test</div>;
   return (
-    <Container>
-      <Input
-        $icon={<Icons.Houses />}
-        placeholder={"Enter an address, neighborhood, city, or ZIP code"}
-      />
+    <div>
+      <Container>
+        <Input
+          $icon={<Icons.Houses />}
+          placeholder={"Enter an address, neighborhood, city, or ZIP code"}
+        />
 
-      <Dropdown
-        menu={{ menu }}
-        placement="bottomRight"
-        arrow={{ pointAtCenter: true }}>
-        <div>
-          <Button type={"light"}>
-            <Icons.Settings /> Advanced
-          </Button>
-        </div>
-        <Button>bottomRight</Button>
-      </Dropdown>
-      <Button>
-        <Icons.Search />
-        Search
-      </Button>
-    </Container>
+        <Dropdown
+          menu={{ menu }}
+          placement="bottomRight"
+          arrow={{ pointAtCenter: true }}>
+          <div>
+            <Button type={"light"}>
+              <Icons.Settings /> Advanced
+            </Button>
+            {/* <Button>bottomRight</Button> */}
+          </div>
+        </Dropdown>
+        <Button>
+          <Icons.Search />
+          Search
+        </Button>
+      </Container>
+    </div>
   );
 };
 
