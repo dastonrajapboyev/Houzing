9aa828195a16b527794f21736e647b428e33f618
dastonrajapboyev <dastonrajapboyev79@gmail.com>
2023-10-25 22:51:15 -0700

    slider created

--- src/components/Carousel/index.jsx
+++ src/components/Carousel/index.jsx
@@ -1,10 +1,9 @@
 import React, { useRef } from "react";
-import { Container, Arrow, Img } from "./style";
+import { Container, Arrow, Img, Blur, Content } from "./style";
 import { Carousel } from "antd";
 
-import Img1 from '../../assets/images/house1.png'
-import Img2 from '../../assets/images/house2.png'
-
+import Img1 from "../../assets/images/house1.png";
+import Img2 from "../../assets/images/house2.png";
 
 const GenCarousel = () => {
   const slider = useRef();
@@ -21,9 +20,15 @@ const GenCarousel = () => {
   return (
     <Container>
       <Carousel ref={slider} afterChange={onChange}>
-       <Img src={Img1}/>
-       <Img src={Img2}/>
+        <Img src={Img1} />
+        <Img src={Img2} />
       </Carousel>
+      <Blur />
+      <Content>
+        <Content.Title>Skyper Pool Partment</Content.Title>
+        <Content.Desc className='subChild'>112 Glenwood Ave Hyde Park, Boston, MA</Content.Desc>
+        <Content.Price>$5,250/mo</Content.Price>
+      </Content>
       <Arrow onClick={onMove} name="left" data-name="left" direction="left" />
       <Arrow
         onClick={onMove}
--- src/components/Carousel/style.js
+++ src/components/Carousel/style.js
@@ -12,7 +12,7 @@ const Arrow = styled(arrow)`
   cursor: pointer;
   padding: 18px;
   border-radius: 50%;
-  background-color: rgba(0, 0, 0, 0.2);
+  background-color: rgba(255, 255, 255, 0.2);
   transform: ${({ direction }) =>
     direction === "left" ? "rotate(-180deg)" : "rotate(0deg)"};
   left: ${({ direction }) => direction === "right" && "20px"};
@@ -23,8 +23,52 @@ const Arrow = styled(arrow)`
 `;
 
 const Img = styled.img`
-width: 100%;
-height: 100%;
-`
+  width: 100%;
+  height: 100%;
+`;
 
-export { Container, Arrow, Img };
+const Blur = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  background: rgba(0, 0, 0, 0.3);
+`;
+const Content = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+
+  display: flex;
+  flex-direction: column;
+  color: #fff;
+  justify-content: center;
+  align-items: center;
+`;
+Content.Title = styled.h1`
+  color: #fff;
+  font-size: 44px;
+  font-style: normal;
+  font-weight: 700;
+  letter-spacing: -0.02px;
+`;
+Content.Desc = styled.div`
+  color: #fff;
+  font-size: 16px;
+  font-weight: 400;
+  line-height: 24px;
+`;
+Content.Price = styled.div`
+  color: #fff;
+  font-family: Montserrat;
+  font-size: 28px;
+  font-style: normal;
+  font-weight: 600;
+  line-height: 36px;
+  letter-spacing: -0.02px;
+`;
+
+export { Container, Arrow, Img, Blur, Content };
