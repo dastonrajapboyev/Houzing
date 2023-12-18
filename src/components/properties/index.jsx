1631cbbc96f45503522d45997cdd603675dc6b52
dastonrajapboyev <dastonrajapboyev79@gmail.com>
2023-12-19 00:16:14 +0500

    signin page: url, request were finished, properties page is finished

--- src/components/properties/index.jsx
+++ src/components/properties/index.jsx
@@ -2,9 +2,9 @@ import React, { useEffect, useState } from "react";
 import { Container } from "./style";
 import HouseCard from "../HouseCard";
 import { useLocation, useNavigate } from "react-router-dom";
-// import useRequest from "../../hooks/useRequest";
 
 const Properties = () => {
+  
   const [data, setData] = useState([]);
   const { search } = useLocation();
   const navigate = useNavigate();
@@ -45,54 +45,3 @@ const Properties = () => {
 };
 
 export default Properties;
-
-// ======================
-
-// import React, { useEffect, useState } from "react";
-// import { Container } from "./style";
-// import HouseCard from "../HouseCard";
-// import { useLocation, useNavigate } from "react-router-dom";
-// import useRequest from "../../hooks/useRequest"; // Assuming this is your custom hook for making requests
-
-// const Properties = () => {
-//   const [data, setData] = useState([]);
-//   const { search } = useLocation();
-//   const navigate = useNavigate();
-//   const  request  = useNavigate(); // Correcting the assignment here assuming `request` is provided by your custom hook
-
-//   useEffect(() => {
-//     const { REACT_APP_BASE_URL: url } = process.env;
-//     // Using the request function from the custom hook
-//     fetch(`${url}/houses/list${search}`)
-//       .then((res) =>
-//         res.json().then((res)=>{
-
-//           setData(res?.data || []);
-//         })
-//         // console.log(res);
-//       )
-//       .catch((error) => {
-//         // Handle errors here
-//         console.error("Error fetching data:", error);
-//       });
-//  request({url:`/houses/list${search}`})
-//   }, [search]);
-
-//   const onSelect = (id) => {
-//     navigate(`/properties/${id}`);
-//   };
-
-//   return (
-//     <Container>
-//       {data.map((value) => (
-//         <HouseCard
-//           onClick={() => onSelect(value.id)}
-//           key={value.id}
-//           data={value}
-//         />
-//       ))}
-//     </Container>
-//   );
-// };
-
-// export default Properties;
